fc5f52120c6108db4b55ab1bfb5836d20faff5c0
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-05-25 12:48:51 +0300

    using Link

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React,{ useTransition, useState } from 'react'
+import Link from 'next/link';
 import Image from 'next/image'
 import TabButton from './TabButton';
 import Skills from './Skills';
@@ -43,13 +44,13 @@ const About = () => {
         <div className='mt-4 lg:mt-0 text-left flex flex-col h-full'>
           <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
           <p className='text-base lg:text-lg'>
-            I'm a Software Engineer driven by a passion for creating solutions that make a real difference. 
-            Specializing in web development and machine learning, I bring a blend of technical expertise and creativity to every project. 
-            Currently, I'm proud to be part of the talented team at <a href={"https://www.linkedin.com/company/ElitesXTech"}>ElitesX.Tech</a>
-            while also honing my skills as a Data Science intern at ExploreAI.
-            I'm always on the lookout for exciting challenges and opportunities to learn and grow. 
-            If you're interested in collaborating, I'd love to connect and explore how we can work together to bring your ideas to life.     
-          </p>
+          I'm a Software Engineer driven by a passion for creating solutions that make a real difference. 
+          Specializing in web development and machine learning, I bring a blend of technical expertise and creativity to every project. 
+          Currently, I'm proud to be part of the talented team at<Link className='hover:text-white text-[#ADB7BE]' href="https://www.linkedin.com/company/ElitesXTech"> ElitesX.Tech </Link> 
+          while also honing my skills as a Data Science intern at ExploreAI. 
+          I'm always on the lookout for exciting challenges and opportunities to learn and grow. 
+          If you're interested in collaborating, I'd love to connect and explore how we can work together to bring your ideas to life.
+         </p>
           <div className='flex flex-row mt-8'>
             <TabButton selectTap={() => handleTapChange("skills")} active={tab === "skills"}>Skills</TabButton>
             <TabButton selectTap={() => handleTapChange("education")} active={tab === "education"}>Education</TabButton>
