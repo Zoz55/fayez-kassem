86f949d8f13c782ed704cb7c0f663583874bc2f2
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2024-05-26 19:26:07 +0300

    cahnged project tag styles

--- app/components/ProjectTag.jsx
+++ app/components/ProjectTag.jsx
@@ -6,7 +6,7 @@ const ProjectTag = ({ name, onClick, isSelected }) => {
   : "text-[#ADB7BE] border-slate-600 hover:border-white";
   return (
     <button
-    className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+    className={`${buttonStyles} rounded-full border-2 px-3 lg:px-6 py-1 lg:py-3 lg:text-xl cursor-pointer`}
     onClick={() => onClick(name)}
   >
     {name}
