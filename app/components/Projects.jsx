d4a6cd5b3e368f734cc4ba2fefa9c472038b5593
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2024-05-26 22:32:45 +0300

    added navigation bar links

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -19,7 +19,7 @@ const Projects = () => {
 
   return (
     <>
-      <div className='w-full'>
+      <div className='w-full' id='projects'>
 
         <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
           My Projects
