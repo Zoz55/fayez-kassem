6fe4639edd8df3654d55c51fb23fcf3de79d7727
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2024-05-26 19:26:46 +0300

    added  div around projects and set project taps as a grid

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -19,41 +19,43 @@ const Projects = () => {
 
   return (
     <>
-      <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
-        My Projects
-      </h2>
+      <div className='w-full'>
 
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <ProjectTag
-          onClick={handleTagChange}
-          name="All"
-          isSelected={tag === "All"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="FullStack"
-          isSelected={tag === "FullStack"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="FrontEnd"
-          isSelected={tag === "FrontEnd"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="BackEnd"
-          isSelected={tag === "BackEnd"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="ConsoleApp"
-          isSelected={tag === "ConsoleApp"}
-        />
-      </div>
+        <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
+          My Projects
+        </h2>
 
-      <div className='grid lg:grid-cols-3 gap-8 lg:gap-12'>
-        {filteredProjects.map((project) =>(
-          <ProjectCard
+        <div className="text-white grid grid-cols-2 justify-center items-center gap-2 py-6 lg:flex lg:flex-row">
+          <ProjectTag
+            onClick={handleTagChange}
+            name="All"
+            isSelected={tag === "All"}
+            />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="FullStack"
+            isSelected={tag === "FullStack"}
+            />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="FrontEnd"
+            isSelected={tag === "FrontEnd"}
+            />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="BackEnd"
+            isSelected={tag === "BackEnd"}
+            />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="ConsoleApp"
+            isSelected={tag === "ConsoleApp"}
+            />
+        </div>
+
+        <div className='grid lg:grid-cols-3 gap-8 lg:gap-12'>
+          {filteredProjects.map((project) =>(
+            <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
@@ -61,8 +63,9 @@ const Projects = () => {
             gitUrl={project.gitUrl}
             previewUrl={project.previewUrl}
             technologies={project.technologies}
-          />
-        ))}
+            />
+          ))}
+        </div>
       </div>
     </>
   )
